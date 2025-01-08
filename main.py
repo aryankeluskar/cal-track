1475d60c10c275ec618ae03ddde93666f53f6254
Aryan Keluskar <aryankeluskar6@gmail.com>
2025-01-07 23:37:09 -0700

    Add index route with daily totals display

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from database import get_daily_totals
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    totals = get_daily_totals()
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "calories": totals['calories'],
+            "carbs": totals['carbs'],
+            "protein": totals['protein']
+        }
+    )
 
 if __name__ == "__main__":
     import uvicorn
